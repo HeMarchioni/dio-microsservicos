b4aacf7205045239c5b2883171e89261ee9b5604
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-08-11 02:02:56 -0300

    adding gateway

---
+++ gateway/src/test/java/dio/experts/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.experts.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
